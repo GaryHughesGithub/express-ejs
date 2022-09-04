3db968134c68f1fdc7df31ca077d2cf4b4cb5e06
Gary Hughes <gclm.automation@outlook.com>
2022-09-04 22:19:29 +0100

    added static files and how to use

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const redditData = require('./data.json');
+
+app.use(express.static(path.join(__dirname, 'public'))); //directory to group file css/js/imgs etc
+                                   // this can be seperated out into seperate directories
+
 
 app.set('view engine', 'ejs');
 app.set('views',path.join(__dirname, '/views'));
@@ -18,10 +23,29 @@ app.get('/rand', (req,res) => {
   res.render('random', {rand: num});
 })
 
+// app.get('/r/:subreddit', (req, res) => {
+//   const {subreddit} = req.params;
+//   res.render('subreddit', {subreddit});
+// })
+
 app.get('/r/:subreddit', (req, res) => {
   const {subreddit} = req.params;
-  res.render('subreddit', {subreddit});
+  const data = redditData[subreddit];
+  if(data) {
+    res.render('subreddit', {...data}); //SPREADING THE DATA FROM JSON FILE
+  } else {
+    res.render('notfound', {subreddit});
+  }
+  })
+  
+
+app.get('/cats', (req, res) => {
+  const cats = [
+    'Blue', 'Alan', 'Trevor'
+  ]
+  res.render('cats', {allCats : cats})
 })
+
 
 app.get('/', (req, res) => {
   res.send('Sorry no path was chosen!')
